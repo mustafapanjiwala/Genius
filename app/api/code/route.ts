b6ccf76b3343a2ea4e45dcc7b974b39863a89e79
Mustafa Panjiwala <56435815+mustafapanjiwala@users.noreply.github.com>
2023-11-11 22:14:32 +0530

    second commit

---
+++ app/api/code/route.ts
@@ -0,0 +1,42 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import OpenAI from "openai";
+import { ChatCompletionMessageParam } from "openai/resources";
+("openai/resources");
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const instructionMessage: ChatCompletionMessageParam = {
+  role: "system",
+  content:
+    "you are acode generator. You must answer only in markdown code snippets. Use code comments for explanations",
+};
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!openai.apiKey) {
+      return new NextResponse("OpenAI API key not configured", { status: 500 });
+    }
+    if (!messages) {
+      return new NextResponse("No messages provided", { status: 400 });
+    }
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [instructionMessage, ...messages],
+    });
+
+    return NextResponse.json(response.choices[0].message);
+  } catch (err) {
+    console.log("[CODE_ERROR]", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
---
+++ components/loader.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export const Loader = () => {
+  return (
+    <div className="flex flex-col gap-y-4 items-center justify-center h-full">
+      <div className="w-10 h-10 relative animate-spin">
+        <Image alt="Logo" fill src="/logo.png" />
+      </div>
+      <div className="text-sm text-muted-foregoround">
+        Genius is thinking ...
+      </div>
+    </div>
+  );
+};
